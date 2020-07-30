ad03304b58f7c6685d173ff55a32b195e9708fb2
ndev <napodevesa@gmail.com>
2020-07-30 16:32:40 +0200

    modifications color design CSS

--- app.js
+++ app.js
@@ -1,94 +1,116 @@
 // Tutorial by http://youtube.com/CodeExplained
 // api key : 82005d27a116c2880c8f0fcb866998a0
-
-
-// SELECT ELEMENTS
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 const notificationElement = document.querySelector(".notification");
 
-// App data
-const weather = {};
+const weather  = {};
 
-weather.temperature = {
-    unit : "celsius"
+weather.temperature= {
+    unit: "celsius"
 }
 
-// APP CONSTS AND VARS
 const KELVIN = 273;
-// API KEY
-const key = "82005d27a116c2880c8f0fcb866998a0";
 
-// CHECK IF BROWSER SUPPORTS GEOLOCATION
+const KEY = "c8822e1d64d60c757a04a7bdf93b6dea";
+
+//Sacando la ubicacion desde la IP
+function ipLookUp () {
+    $.ajax('http://ip-api.com/json')
+    .then(
+        function success(response) {
+            let latitude  =  response.lat 
+            let longitude = response.lon
+
+            getWeather(latitude, longitude)
+        },
+  
+        function fail(data, status) {
+            console.log('Request failed.  Returned status of',
+                        status);
+        },
+    );
+  }
+
+//CHECK GEOLOCATION 
+
 if('geolocation' in navigator){
-    navigator.geolocation.getCurrentPosition(setPosition, showError);
+    navigator.geolocation.getCurrentPosition(setPosition,ipLookUp)
 }else{
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
+    ipLookUp();
 }
-
-// SET USER'S POSITION
+//CHECK USER 
 function setPosition(position){
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
-    
+
     getWeather(latitude, longitude);
 }
-
-// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
 function showError(error){
     notificationElement.style.display = "block";
     notificationElement.innerHTML = `<p> ${error.message} </p>`;
 }
 
-// GET WEATHER FROM API PROVIDER
+//GET WEATHER FROM API
 function getWeather(latitude, longitude){
-    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-    
+    let api = ` https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${KEY}`;
+   
     fetch(api)
         .then(function(response){
             let data = response.json();
             return data;
         })
         .then(function(data){
-            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-            weather.description = data.weather[0].description;
-            weather.iconId = data.weather[0].icon;
-            weather.city = data.name;
-            weather.country = data.sys.country;
+            weather.temperature.value = Math.floor(data.current.temp - KELVIN);
+            weather.description = data.current.weather[0].description;
+            weather.iconId = data.current.weather[0].icon;
+            weather.locat = data.timezone;
+
         })
         .then(function(){
             displayWeather();
         });
+
+        function displayWeather(){
+            iconElement.innerHTML = `<img src="icons/${weather.iconId}.png">`;
+            tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
+            descElement.innerHTML = `${weather.description}`;
+            locationElement.innerHTML = `${weather.locat}`
+        }
 }
 
-// DISPLAY WEATHER TO UI
-function displayWeather(){
-    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
-    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-    descElement.innerHTML = weather.description;
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
-}
+//convercion de C a F
 
-// C to F conversion
-function celsiusToFahrenheit(temperature){
+function celsiusToFahrenhait(temperature){
     return (temperature * 9/5) + 32;
 }
 
-// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
+//convercion de F a C
+
+function FahrenhaitToCelsius(temperature){
+    return (temperature - 32) *  5/9;
+}
+
+//El listener del Click
+
 tempElement.addEventListener("click", function(){
     if(weather.temperature.value === undefined) return;
-    
+
     if(weather.temperature.unit == "celsius"){
-        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
-        fahrenheit = Math.floor(fahrenheit);
-        
-        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
-        weather.temperature.unit = "fahrenheit";
+        let fahrenhait = celsiusToFahrenhait(weather.temperature.value);
+        fahrenhait = Math.floor(fahrenhait);
+        weather.temperature.value = fahrenhait;
+
+        tempElement.innerHTML = `${weather.temperature.value}°<span>F</span>`;
+        weather.temperature.unit = "fahrenhait";
     }else{
+        let celsius = FahrenhaitToCelsius(weather.temperature.value);
+        celsius = Math.ceil(celsius);
+        weather.temperature.value = celsius;
+
         tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-        weather.temperature.unit = "celsius"
+        weather.temperature.unit = "celsius";
     }
-});
+})
